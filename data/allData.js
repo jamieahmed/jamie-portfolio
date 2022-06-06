9fd77f41efadf2104a5d9735c0607d58f70a02a1
Jamie Ahmed <jamieahmed273@gmail.com>
2022-06-05 20:30:29 -0400

    add favicon pic

---
+++ data/allData.js
@@ -0,0 +1,15 @@
+const projects = [{
+    id: "f1",
+    category: "Front-end",
+    title: "Tic-Tac-Toe",
+    image: "../portfolio/assets/Tic-Tac-Toe.png",
+    url: "https://jamie-tic-tac-toe-game.netlify.app/",
+    status: "completed",
+    description:
+        "This is Tic-Tac-Toe game ,",
+    languages: "HTML,CSS,JavaScript, DOM.",
+}]
+
+export {
+    projects
+}
---
+++ js/app.js
@@ -0,0 +1,2 @@
+import { projects } from "../data/allData";
+
